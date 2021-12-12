9edce80950f044587b6c25fe81049fa98f4a29c8
Gabriel Jestin <jestingabriel@gmail.com>
2021-12-12 20:42:37 +0100

    day 01 OK

--- 01-sonar-sweep/day01.py
+++ 01-sonar-sweep/day01.py
@@ -1,1 +1,23 @@
 import requests
+
+depths = list()
+
+with open("raw_data") as raw_data:
+    for line in raw_data.readlines():
+        depths.append(int(line))
+
+count_greater_depths = 0
+for i in range(len(depths) - 1):
+    if depths[i] < depths[i+1]:
+        count_greater_depths += 1
+
+print("Answer part one:", count_greater_depths)
+
+count_greater_depths_sliding_window = 0
+for i in range(len(depths) - 1):
+    current_window = sum(depths[i:i+3])
+    next_window = sum(depths[i+1:i+4])
+    if current_window < next_window:
+        count_greater_depths_sliding_window += 1
+
+print("Answer part two:", count_greater_depths_sliding_window)
